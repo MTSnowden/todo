d7399231ec3b0bcd5a116af66105debb29a8521b
Michael Snowden <mtsnowden@gmail.com>
2019-06-18 17:46:59 -0500

    commenting

---
+++ todo/src/todoList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css'
+
+export default class TodoList extends Component {
+   render() {
+       return(
+        <div>
+            <div id='title-container'>
+                <h1>To-Do List</h1>
+            </div>
+            <div id="input-container">
+                <input id="input-box" placeholder="WHATCHA GOTTA DO?"></input>
+                <button id='input-button' placeholder="ADD"></button>
+            </div>
+            <div><button id='input-button' placeholder="ADD"></button></div>
+        </div>
+       )
+   }
+}
+
